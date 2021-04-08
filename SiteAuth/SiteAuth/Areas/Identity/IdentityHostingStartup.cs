592a6eed37ba6a7c2c4987c2b81c9d8ba1a3d492
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-04-08 14:21:07 +0300

    Create Log in

---
+++ SiteAuth/SiteAuth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SiteAuth.Areas.Identity.Data;
+using SiteAuth.Data;
+
+[assembly: HostingStartup(typeof(SiteAuth.Areas.Identity.IdentityHostingStartup))]
+namespace SiteAuth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<SiteUser>(options =>
+                    {
+                        options.SignIn.RequireConfirmedAccount = false;
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                    })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
