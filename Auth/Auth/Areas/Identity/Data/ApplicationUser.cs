6f3746ba7ba4fb4e459c7f693e4bd4b93a4ed5eb
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-04-15 15:24:29 +0300

    Create Login and Reg

---
+++ Auth/Auth/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Auth.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName{ get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string City{ get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public int Age{ get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(1)")]
+        public char Gender { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(250)")]
+        public string AboutMyself { get; set; }
+    }
+}
---
+++ Auth/Auth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Auth.Areas.Identity.Data;
+using Auth.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Auth.Areas.Identity.IdentityHostingStartup))]
+namespace Auth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                    {
+                        options.SignIn.RequireConfirmedAccount = false;
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                    })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
