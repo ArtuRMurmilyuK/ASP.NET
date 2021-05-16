5118cd5e2851716c5392918026c9c21faf4218a6
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-05-16 21:08:32 +0300

    Create the Empty project

---
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index(){
+        
+            Dictionary<string, Product> products = new Dictionary<string, Product>
+                {
+                    ["Kayak"] = new Product{Name = "Kayak", Price = 275M},
+                    ["Lifejacket"] = new Product{Name = "Lifejacket", Price = 48.95M}
+                };
+            return View("Index", products.Keys);
+        }
+    }
+}
