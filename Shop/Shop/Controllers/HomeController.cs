6094fd58a9eed5bf6141fe438a14b3363a15f57e
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2021-05-28 16:29:33 +0300

    Add HomeController

---
+++ Shop/Shop/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllCars _carRep;
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                FavCar = _carRep.GetFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
