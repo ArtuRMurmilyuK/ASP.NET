54c35ce4f2258fc6e7122dfbe45a03654a88a303
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-05-01 20:39:08 +0300

    Added ListController

---
+++ ITVDN_UserInterface/HelloApp/HelloApp/Controllers/ListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloApp.Controllers
+{
+    public class ListController : Controller
+    {
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
