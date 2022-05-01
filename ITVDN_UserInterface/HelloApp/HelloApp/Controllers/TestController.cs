54ae3b39152a900001f822d90ec0002f965297fc
ArtuRMurmilyuK <murmilyuk.artur20@gmail.com>
2022-05-01 20:25:50 +0300

    Created project and add TestController

---
+++ ITVDN_UserInterface/HelloApp/HelloApp/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using HelloApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloApp.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
